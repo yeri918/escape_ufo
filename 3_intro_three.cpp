fff54b4fcf2835ca7a2ef81494eebb14373dfc74
yeri918 <61730858+yeri918@users.noreply.github.com>
2020-05-05 04:21:04 +0800

    Delete 3_intro_three.cpp

--- 3_intro_three.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "Game3.h"
-using namespace std;
-void intro_three(){
-  cout << "ROUND THREE\n";
-  cout << "You have to avoid all the aliens and escape through the door." << endl;
-}
