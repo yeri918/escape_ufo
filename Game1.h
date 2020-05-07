8f37ac9d6a734c21a16d229cd9d8d21a0c811f9c
TaeyeonYEE <62612789+TaeyeonYEE@users.noreply.github.com>
2020-05-07 15:54:41 +0900

    Add files via upload

--- Game1.h
+++ Game1.h
@@ -1,17 +1,35 @@
+#pragma once
 #ifndef GAME_1
-#define GAME_1
+#define Game_1
 
 #include <iostream>
 using namespace std;
 
-char m[5][14]={{'U','s','e',' ','c','a','r','d',' ','o','n','e'},
-              {'U','s','e',' ','c','a','r','d',' ','t','w','o'},
-              {'U','s','e',' ','c','a','r','d',' ','t','h','r','e','e'},
-              {'U','s','e',' ','c','a','r','d',' ','f','o','u','r'},
-              {'U','s','e',' ','c','a','r','d',' ','f','i','v','e'}};
+class counting 
+{
+public:
+	int f_fan = 0, f_machine = 0, f_dlc = 0, f_card = 0, f_correct = 0, f_open = 0;
+};
 
-int find_card();
-void find_translator();
-int Game1();
+void Game1();
+// Introduction for the game 1
+void introdialog();
+// Convert messege to encrypted ones
+string encryption(string messege, counting count);
+// Main menu for the user: it decides what to do
+void action(counting &count);
+// Wall and the stain
+void wall(counting count);
+// Interactions that can happen to bed
+void bed(counting &count);
+// Encrypts the messege given. Only used before the machine is found
+void machine(counting &count);
+// Interactions that can happen to bookshelf
+void bookshelf(counting count);
 
-#endif GAME_1
+void cardfind(counting count, int place);
+
+void table(counting &count);
+
+void door(counting& count);
+#endif // GAME_1
